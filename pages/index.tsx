6b355a624b9e6567177d8a0ce88f2d7271eb81a1
zakiah <zakiah_bloom@yahoo.com>
2022-11-27 20:43:52 +0800

    videocard updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
-import type { NextPage } from "next";
-import axios from "axios";
+// import axios from "axios";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import { Video } from "../types";
+import React from "react";
+import { NextPage } from "next";
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos?.map((video: Video) => (
+          <VideoCard
+            post={video}
+            key={video._id}
+          />
+        ))
+      ) : (
+        <NoResults text={`No Videos`} />
+      )}
+    </div>
+  );
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
-
-  console.log(response.data.name);
-
+  const res = await fetch(`http://localhost:3000/api/post`);
+  const data = await res.json();
   return {
-    props: {},
+    props: {
+      videos: data,
+    },
   };
 };
 
